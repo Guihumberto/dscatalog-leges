85a5cc6daab5bd18930b894465d94131220696f5
guihumberto <juninho.joao@hotmail.com>
2022-12-01 08:49:44 -0300

    h2-console config

---
+++ backend/src/main/java/com/leges/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.leges.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leges.dscatalog.entities.Category;
+import com.leges.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
